6cd4f435031af02d3e50343ba0a22da5fc56701f
Gian Carlo Mantuan <giancarlo.mdr@gmail.com>
2021-04-16 01:21:44 -0300

    refactor: change api call from component to service handler

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,12 @@
 import axios from 'axios';
 
 export const api = axios.create({baseURL: 'https://api.pokemontcg.io/v1'});
+
+export const getCards = async () => {
+  const {data} = await api.get('/cards')
+  if (data) {
+    return data.cards
+  }
+
+  return []
+} 
